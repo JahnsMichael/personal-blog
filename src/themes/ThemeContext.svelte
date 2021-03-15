99c1135ea20516b07c2a49fbe2b05857e6374f1a
jahnsmichael <michael.jf.jm@gmail.com>
2021-03-16 00:48:00 +0700

    add prefers color scheme detection

--- src/themes/ThemeContext.svelte
+++ src/themes/ThemeContext.svelte
@@ -6,7 +6,7 @@
   export let themes = [...presets];
 
   // Utility
-  const setRootColors = theme => {
+  const setRootColors = (theme) => {
     for (let [prop, color] of Object.entries(theme.colors)) {
       let varString = `--theme-${prop}`;
       document.documentElement.style.setProperty(varString, color);
@@ -14,8 +14,8 @@
     document.documentElement.style.setProperty("--theme-name", theme.name);
   };
 
-  const getThemeByName = name => themes.find(el => el.name === name);
-  
+  const getThemeByName = (name) => themes.find((el) => el.name === name);
+
   // Main
   let _current = themes[0].name;
   const Theme = writable(getThemeByName(_current));
@@ -23,16 +23,24 @@
   setContext("theme", {
     theme: Theme,
     toggle: () => {
-      let _currentIndex = themes.findIndex(el => el.name === _current);
-      _current = themes[(_currentIndex === themes.length - 1) ? 0 : (_currentIndex += 1)].name;
-      Theme.update(t => ({ ...t, ...getThemeByName(_current) }));
+      let _currentIndex = themes.findIndex((el) => el.name === _current);
+      _current =
+        themes[_currentIndex === themes.length - 1 ? 0 : (_currentIndex += 1)]
+          .name;
+      Theme.update((t) => ({ ...t, ...getThemeByName(_current) }));
       setRootColors(getThemeByName(_current));
-    }
-  })
+    },
+    setTheme: (themeName) => {
+      Theme.update((t) => ({ ...t, ...getThemeByName(themeName) }));
+      setRootColors(getThemeByName(themeName));
+    },
+  });
 
   onMount(() => {
+    const darkMode = window.matchMedia("(prefers-color-scheme: dark)")
+    _current = darkMode.matches ? "dark" : "light";
     setRootColors(getThemeByName(_current));
-  })
+  });
 </script>
 
-<slot></slot>
+<slot />
